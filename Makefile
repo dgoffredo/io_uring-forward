b1fca581333232eccd32522da02c2c810285392e
David Goffredo <dmgoffredo@gmail.com>
2025-02-14 11:18:55 -0500

    works, but short reads are a fundamental problem

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-echo-server: echo-server.cpp echo-server.flags
-	$(CXX) $(file < echo-server.flags) -o $@ $<
+echo-server: echo-server.cpp echo-server.cflags echo-server.lflags
+	$(CXX) $(file < echo-server.cflags) -o $@ $< $(file < echo-server.lflags)
+
+.PHONY: format
+format:
+	clang-format -i --style='{BasedOnStyle: Google, Language: Cpp, ColumnLimit: 80}' *.cpp
--- echo-server.cpp
+++ echo-server.cpp
@@ -4,49 +4,89 @@ extern "C" {
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-} // extern "C"
+}  // extern "C"
+
+#include <liburing.h>
 
 #include <cerrno>
 #include <cstring>
 #include <iostream>
 
-#define REQUIRE(NAME, EXPR)                                                    \
-  if (-1 == (EXPR)) {                                                          \
-    const int err = errno;                                                     \
-    std::cout << NAME << " failed with: " << std::strerror(err) << '\n';       \
-    return err;                                                                \
+#define POSIX_REQUIRE(NAME, EXPR)                                \
+  if (-1 == (EXPR)) {                                            \
+    const int err = errno;                                       \
+    std::cout << __LINE__ << ": " << NAME                        \
+              << " failed with: " << std::strerror(err) << '\n'; \
+    return err;                                                  \
+  }
+
+#define URING_REQUIRE(NAME, EXPR)                                 \
+  if (const int err = (EXPR); err < 0) {                          \
+    std::cout << __LINE__ << ": " << NAME                         \
+              << " failed with: " << std::strerror(-err) << '\n'; \
+    return -err;                                                  \
+  }
+
+#define PTR_REQUIRE(NAME, EXPR)                           \
+  if (!(EXPR)) {                                          \
+    std::cout << __LINE__ << ": " << NAME << " failed\n"; \
+    return -1;                                            \
   }
 
 int main() {
-  int listenfd, connfd;
+  int listenfd = -1, connfd = -1;
+  int pipefds[2] = {-1, -1};
   sockaddr_in serv_addr = {};
-  char buf[1025] = {};
-  ssize_t count;
 
-  REQUIRE("socket()", listenfd = socket(AF_INET, SOCK_STREAM, 0));
+  io_uring ring;
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(1337);
+  const int rc = [&]() {
+    URING_REQUIRE("io_uring_queue_init()", io_uring_queue_init(8, &ring, 0));
+    POSIX_REQUIRE("pipe()", pipe(pipefds));
+    POSIX_REQUIRE("socket()", listenfd = socket(AF_INET, SOCK_STREAM, 0));
 
-  REQUIRE("bind()",
-          bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
-  REQUIRE("listen()", listen(listenfd, 10));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(1337);
 
-  REQUIRE("accept()", connfd = accept(listenfd, NULL, NULL));
+    POSIX_REQUIRE("bind()", bind(listenfd, (struct sockaddr *)&serv_addr,
+                                 sizeof(serv_addr)));
+    POSIX_REQUIRE("listen()", listen(listenfd, 10));
 
-  for (;;) {
-    // TODO: use io_uring instead
+    POSIX_REQUIRE("accept()", connfd = accept(listenfd, NULL, NULL));
 
-    REQUIRE("recv()", count = recv(connfd, buf, sizeof buf, 0));
-    if (count == 0) {
-      std::cout << "No more data to read from connection.\n";
-      return 0;
+    for (;;) {
+      constexpr int splice_size = 3;
+      io_uring_sqe *sqe;
+      io_uring_cqe *cqe;
+
+      PTR_REQUIRE("io_uring_get_sqe()", sqe = io_uring_get_sqe(&ring));
+      io_uring_prep_splice(sqe, connfd, -1, pipefds[1], -1, splice_size, 0);
+      sqe->flags |= IOSQE_IO_LINK;
+
+      PTR_REQUIRE("io_uring_get_sqe()", sqe = io_uring_get_sqe(&ring));
+      io_uring_prep_splice(sqe, pipefds[0], -1, connfd, -1, splice_size, 0);
+
+      io_uring_submit(&ring);
+
+      for (int i = 0; i < 2; i++) {
+        URING_REQUIRE("io_uring_wait_cqe()", io_uring_wait_cqe(&ring, &cqe));
+        URING_REQUIRE("cqe->res", cqe->res);
+        std::cout << "Result of the operation i=" << i << ": " << cqe->res
+                  << '\n';
+        io_uring_cqe_seen(&ring, cqe);
+      }
     }
 
-    ssize_t sent = 0;
-    do {
-      REQUIRE("send()", sent += send(connfd, buf + sent, count - sent, 0));
-    } while (count != sent);
+    return 0;
+  }();
+
+  io_uring_queue_exit(&ring);
+  for (const int fd : {connfd, listenfd, pipefds[0], pipefds[1]}) {
+    if (fd >= 0) {
+      close(fd);
+    }
   }
+
+  return rc;
 }

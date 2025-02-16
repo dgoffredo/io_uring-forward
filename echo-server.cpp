48279bcf4c96b7788740b9cc03b57e6196ccb032
David Goffredo <dmgoffredo@gmail.com>
2025-02-16 14:16:37 -0500

    tee() to another socket

--- echo-server.cpp
+++ echo-server.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <liburing.h>
 
 #include <cerrno>
+#include <cstdint>
 #include <cstring>
 #include <iostream>
 
@@ -34,26 +35,46 @@ extern "C" {
   }
 
 int main() {
-  int listenfd = -1, connfd = -1;
-  int pipefds[2] = {-1, -1};
+  int listen1fd = -1, conn1fd = -1;
+  int pipe1fds[2] = {-1, -1};
+  int listen2fd = -1, conn2fd = -1;
+  int pipe2fds[2] = {-1, -1};
   sockaddr_in serv_addr = {};
+  const std::uint16_t echo_port = 1337;
+  const std::uint16_t observer_port = 1338;
 
   io_uring ring;
 
   const int rc = [&]() {
     URING_REQUIRE(io_uring_queue_init(8, &ring, 0));
-    POSIX_REQUIRE(pipe(pipefds));
-    POSIX_REQUIRE(listenfd = socket(AF_INET, SOCK_STREAM, 0));
+    POSIX_REQUIRE(pipe(pipe1fds));
+    POSIX_REQUIRE(pipe(pipe2fds));
+    POSIX_REQUIRE(listen1fd = socket(AF_INET, SOCK_STREAM, 0));
+    POSIX_REQUIRE(listen2fd = socket(AF_INET, SOCK_STREAM, 0));
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(1337);
+    serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    serv_addr.sin_port = htons(echo_port);
 
     POSIX_REQUIRE(
-        bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
-    POSIX_REQUIRE(listen(listenfd, 10));
+        bind(listen1fd, (sockaddr *)&serv_addr, sizeof(serv_addr)));
+    POSIX_REQUIRE(listen(listen1fd, 1));
 
-    POSIX_REQUIRE(connfd = accept(listenfd, NULL, NULL));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    serv_addr.sin_port = htons(observer_port);
+
+    POSIX_REQUIRE(
+        bind(listen2fd, (sockaddr *)&serv_addr, sizeof(serv_addr)));
+    POSIX_REQUIRE(listen(listen2fd, 1));
+
+    std::cout << "Waiting for observer client to connect on port " << observer_port << ".\n";
+    POSIX_REQUIRE(conn2fd = accept(listen2fd, NULL, NULL));
+    std::cout << "Connection established.\n";
+
+    std::cout << "Waiting for echo client to connect on port " << echo_port << ".\n";
+    POSIX_REQUIRE(conn1fd = accept(listen1fd, NULL, NULL));
+    std::cout << "Connection established.\n";
 
     for (;;) {
       constexpr int splice_size = 4096;
@@ -61,26 +82,48 @@ int main() {
       io_uring_cqe *cqe;
 
       PTR_REQUIRE(sqe = io_uring_get_sqe(&ring));
-      io_uring_prep_splice(sqe, connfd, -1, pipefds[1], -1, splice_size, 0);
-      // sqe->flags |= IOSQE_IO_LINK;
+      io_uring_prep_splice(sqe, conn1fd, -1, pipe1fds[1], -1, splice_size, 0);
+      io_uring_sqe_set_data64(sqe, 42);
       sqe->flags |= IOSQE_IO_HARDLINK;
 
       PTR_REQUIRE(sqe = io_uring_get_sqe(&ring));
-      io_uring_prep_splice(sqe, pipefds[0], -1, connfd, -1, splice_size, 0);
-
+      io_uring_prep_tee(sqe, pipe1fds[0], pipe2fds[1], splice_size, 0);
+    
+      std::cout << "Submitting two chained operations to io_uring.\n";
       io_uring_submit(&ring);
 
       for (int i = 0; i < 2; i++) {
+        std::cout << "Waiting for a completion from io_uring.\n";
         URING_REQUIRE(io_uring_wait_cqe(&ring, &cqe));
-        URING_REQUIRE(cqe->res);
+        // URING_REQUIRE(cqe->res);
         const int result = cqe->res;
         std::cout << "Result of the operation i=" << i << ": " << result
                   << '\n';
+        const auto user_data = io_uring_cqe_get_data64(cqe);
         io_uring_cqe_seen(&ring, cqe);
-        if (i == 0 && result == 0) {
+        if (user_data == 42 && result == 0) {
           std::cout << "Nothing more to read.\n";
           return 0;
         }
+      }
+
+      PTR_REQUIRE(sqe = io_uring_get_sqe(&ring));
+      io_uring_prep_splice(sqe, pipe1fds[0], -1, conn1fd, -1, splice_size, 0);
+      
+      PTR_REQUIRE(sqe = io_uring_get_sqe(&ring));
+      io_uring_prep_splice(sqe, pipe2fds[0], -1, conn2fd, -1, splice_size, 0);
+
+      std::cout << "Submitting two unchained operations to io_uring.\n";
+      io_uring_submit(&ring);
+
+      for (int j = 0; j < 2; j++) {
+        std::cout << "Waiting for a completion from io_uring.\n";
+        URING_REQUIRE(io_uring_wait_cqe(&ring, &cqe));
+        URING_REQUIRE(cqe->res);
+        const int result = cqe->res;
+        std::cout << "Result of the operation j=" << j << ": " << result
+                  << '\n';
+        io_uring_cqe_seen(&ring, cqe);
       }
     }
 
@@ -88,7 +131,7 @@ int main() {
   }();
 
   io_uring_queue_exit(&ring);
-  for (const int fd : {connfd, listenfd, pipefds[0], pipefds[1]}) {
+  for (const int fd : {conn1fd, listen1fd, pipe1fds[0], pipe1fds[1], conn2fd, listen2fd, pipe2fds[0], pipe2fds[1]}) {
     if (fd >= 0) {
       close(fd);
     }

eec35ac45d285ffaa55d54d97b6824dcd7a8d78f
David Goffredo <dmgoffredo@gmail.com>
2025-02-21 13:41:45 -0500

    support unix domain sockets and variable buffer size

--- echo-server.cpp
+++ echo-server.cpp
@@ -3,22 +3,28 @@ extern "C" {
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <sys/wait.h>
 #include <unistd.h>
 }  // extern "C"
 
 #include <liburing.h>
+#include <stdlib.h>  // mkdtemp
 
+#include <algorithm>
 #include <bit>
 #include <cerrno>
 #include <chrono>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <ostream>
 #include <sstream>
+#include <string>
+#include <string_view>
 #include <utility>
 
 #define POSIX_REQUIRE(EXPR)                                      \
@@ -44,6 +50,8 @@ extern "C" {
 
 class Net {
  public:
+  virtual ~Net() = default;
+
   // Return a listening socket bound to a local address, or return `-errno` if
   // an error occurs.
   virtual int server_socket(int backlog) = 0;
@@ -52,6 +60,19 @@ class Net {
   // bound, or return `-errno` if an error occurs.
   virtual int client_socket(int server_fd) = 0;
 };
+
+template <typename Address, int family>
+int client_socket(int server_fd) {
+  Address addr = {};
+  socklen_t len = sizeof addr;
+  POSIX_REQUIRE(getsockname(server_fd, (sockaddr *)&addr, &len));
+
+  int sock;
+  POSIX_REQUIRE(sock = socket(family, SOCK_STREAM, 0));
+  POSIX_REQUIRE(connect(sock, (sockaddr *)&addr, sizeof(addr)));
+
+  return sock;
+}
 
 class TCP : public Net {
  public:
@@ -76,19 +97,53 @@ class TCP : public Net {
   }
 
   int client_socket(int server_fd) override {
-    sockaddr_in addr = {};
-    socklen_t len = sizeof addr;
-    POSIX_REQUIRE(getsockname(server_fd, (sockaddr *)&addr, &len));
-
-    int sock;
-    POSIX_REQUIRE(sock = socket(AF_INET, SOCK_STREAM, 0));
-    POSIX_REQUIRE(connect(sock, (sockaddr *)&addr, sizeof(addr)));
-
-    return sock;
+    return ::client_socket<sockaddr_in, AF_INET>(server_fd);
   }
 };
 
-// TODO class Unix : public Net {};
+namespace fs = std::filesystem;
+
+fs::path mktemp_dir() {
+  std::string raw = fs::temp_directory_path() / "echo-server-XXXXXX";
+  if (!mkdtemp(raw.data())) {
+    const int err = errno;
+    std::cerr << "Unable to create directory: " << std::strerror(err) << '\n';
+    std::abort();
+  }
+
+  return raw;
+}
+
+class Unix : public Net {
+  fs::path dir;
+  int counter;
+
+ public:
+  Unix() : dir(mktemp_dir()), counter(0) {}
+
+  ~Unix() { fs::remove_all(dir); }
+
+  int server_socket(int backlog) override {
+    const std::string sockname = dir / ("sock" + std::to_string(++counter));
+
+    int sock;
+    POSIX_REQUIRE(sock = socket(AF_UNIX, SOCK_STREAM, 0));
+
+    sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    const auto size = std::min(sockname.size(), sizeof addr.sun_path);
+    std::copy_n(sockname.data(), size, addr.sun_path);
+
+    POSIX_REQUIRE(bind(sock, (sockaddr *)&addr, sizeof(addr)));
+    POSIX_REQUIRE(listen(sock, backlog));
+
+    return sock;
+  }
+
+  int client_socket(int server_fd) override {
+    return ::client_socket<sockaddr_un, AF_UNIX>(server_fd);
+  }
+};
 
 struct alignas(8) IOEntryContext {
   enum Operation { TEE, SPLICE, SEND, RECV };
@@ -126,11 +181,11 @@ void io_uring_prep(io_uring_sqe *sqe, IOEntryContext io_ctx, int flags = 0,
 }
 
 // Connect and `recv()` continuously, discarding all data.
-int client_sink(Net &net, int server_sock) {
+int client_sink(int bufsize, Net &net, int server_sock) {
   int sock;
   URING_REQUIRE(sock = net.client_socket(server_sock));
 
-  std::vector<char> buffer(4096 * 16);
+  std::vector<char> buffer(bufsize);
   for (;;) {
     int rc;
     POSIX_REQUIRE(rc = recv(sock, buffer.data(), buffer.size(), MSG_TRUNC));
@@ -142,7 +197,7 @@ int client_sink(Net &net, int server_sock) {
 
 // Connect and concurrently `send()` zeros and `recv()`, discarding all
 // received data.
-int client_source_and_sink(Net &net, int server_sock) {
+int client_source_and_sink(int bufsize, Net &net, int server_sock) {
   io_uring ring;
   URING_REQUIRE(io_uring_queue_init(8, &ring, 0));
 
@@ -152,8 +207,8 @@ int client_source_and_sink(Net &net, int server_sock) {
   io_uring_sqe *sqe;
   io_uring_cqe *cqe;
   IOEntryContext io_ctx = {};
-  std::vector<char> buffer(4096 * 16);
-  std::vector<char> payload(4096 * 16);
+  std::vector<char> buffer(bufsize);
+  std::vector<char> payload(bufsize);
 
   const auto prep_send = [&]() {
     sqe = io_uring_get_sqe(&ring);
@@ -211,13 +266,13 @@ int client_source_and_sink(Net &net, int server_sock) {
 // Consume from `conn1fd` and duplicate all data onto `connfd1` and `connfd2`.
 // Use `splice()` and `tee()`, involving the pipes `pipe1fds` and `pipe2fds`,
 // to prevent any copies of data into user space.
-int server_splicetee(io_uring &ring, int conn1fd, int conn2fd,
+int server_splicetee(int bufsize, io_uring &ring, int conn1fd, int conn2fd,
                      int (&pipe1fds)[2], int (&pipe2fds)[2]) {
   const auto interval = std::chrono::seconds(5);
   const auto start = std::chrono::steady_clock::now();
   auto last_flush = start;
   std::uint64_t last_snapshot = 0;
-  constexpr int splice_size = 4096 * 16;
+  const int splice_size = bufsize;
   std::ofstream log("log");
 
   for (std::uint64_t bytes_sent = 0;;) {
@@ -329,14 +384,14 @@ int server_splicetee(io_uring &ring, int conn1fd, int conn2fd,
 
 // Consume from `conn1fd` and duplicate all data onto `connfd1` and `connfd2`.
 // Use `recv()` and `send()` with a buffer in user space.
-int server_recvsend(io_uring &ring, int conn1fd, int conn2fd) {
+int server_recvsend(int bufsize, io_uring &ring, int conn1fd, int conn2fd) {
   const auto interval = std::chrono::seconds(5);
   const auto start = std::chrono::steady_clock::now();
   auto last_flush = start;
   std::uint64_t last_snapshot = 0;
   std::ofstream log("log");
 
-  std::vector<char> buffer(4096 * 16);
+  std::vector<char> buffer(bufsize);
 
   for (std::uint64_t bytes_sent = 0;;) {
     const auto now = std::chrono::steady_clock::now();
@@ -407,16 +462,22 @@ int server_recvsend(io_uring &ring, int conn1fd, int conn2fd) {
 }
 
 void usage(std::ostream &out, const char *argv0) {
-  out << "usage: " << argv0 << " <recvsend | splicetee>\n";
+  out << "usage: " << argv0
+      << " <recvsend | splicetee> <tcp | unix> <#pages>\n"
+         "\nfor example: "
+      << argv0 << " recvsend tcp 16\n";
 }
 
 int main(int argc, char *argv[]) {
   enum { RECVSEND, SPLICETEE } server_mode;
-  if (argc != 2) {
+  std::unique_ptr<Net> net;
+  int bufsize;
+
+  if (argc != 4) {
     usage(std::cerr, argv[0]);
     return 1;
   }
-  const std::string_view arg{argv[1]};
+  std::string_view arg{argv[1]};
   if (arg == "-h" || arg == "--help") {
     usage(std::cout, argv[0]);
     return 0;
@@ -429,27 +490,41 @@ int main(int argc, char *argv[]) {
     usage(std::cerr, argv[0]);
     return 2;
   }
+  arg = argv[2];
+  if (arg == "-h" || arg == "--help") {
+    usage(std::cout, argv[0]);
+    return 0;
+  }
+  if (arg == "tcp") {
+    net = std::make_unique<TCP>();
+  } else if (arg == "unix") {
+    net = std::make_unique<Unix>();
+  } else {
+    usage(std::cerr, argv[0]);
+    return 2;
+  }
+  arg = argv[3];
+  bufsize = std::stoi(std::string{arg}) * getpagesize();
 
   int listen1fd = -1, conn1fd = -1;
   int pipe1fds[2] = {-1, -1};
   int listen2fd = -1, conn2fd = -1;
   int pipe2fds[2] = {-1, -1};
-  TCP net;
 
   io_uring ring;
 
   const int rc = [&]() {
     POSIX_REQUIRE(pipe(pipe1fds));
     POSIX_REQUIRE(pipe(pipe2fds));
-    URING_REQUIRE(listen1fd = net.server_socket(1));
-    URING_REQUIRE(listen2fd = net.server_socket(1));
+    URING_REQUIRE(listen1fd = net->server_socket(1));
+    URING_REQUIRE(listen2fd = net->server_socket(1));
 
     // fork() to client_sink(1338).
     switch (fork()) {
       case 0:
         // child
         // TODO: Should close all file descriptors except 0 and 1, but meh.
-        std::exit(client_sink(net, listen2fd));
+        std::exit(client_sink(bufsize, *net, listen2fd));
       case -1: {
         const int err = errno;
         std::cerr << "error forking to client_sink(1338): "
@@ -463,7 +538,7 @@ int main(int argc, char *argv[]) {
       case 0:
         // child
         // TODO: Should close all file descriptors except 0 and 1, but meh.
-        std::exit(client_source_and_sink(net, listen1fd));
+        std::exit(client_source_and_sink(bufsize, *net, listen1fd));
       case -1: {
         const int err = errno;
         std::cerr << "error forking to client_sink(1338): "
@@ -484,9 +559,10 @@ int main(int argc, char *argv[]) {
 
     switch (server_mode) {
       case RECVSEND:
-        return server_recvsend(ring, conn1fd, conn2fd);
+        return server_recvsend(bufsize, ring, conn1fd, conn2fd);
       case SPLICETEE:
-        return server_splicetee(ring, conn1fd, conn2fd, pipe1fds, pipe2fds);
+        return server_splicetee(bufsize, ring, conn1fd, conn2fd, pipe1fds,
+                                pipe2fds);
       default:
         std::unreachable();
     }
